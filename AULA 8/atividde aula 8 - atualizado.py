9fbbb2ab3910d85d7ac0aff31bf89b10ff4f3f3c
REPESKA <repeskaa@gmail.com>
2026-03-12 15:46:24 -0300

    Add files via upload

---
+++ AULA 8/atividde aula 8 - atualizado.py
@@ -0,0 +1,112 @@
+# 1* 
+print ('1 - Peça para o usuário digitar um número, verifique se um número é positivo, negativo ou zero.')
+
+print(input('DIGITE UM NUMERO')),
+
+
+numero = 100
+
+
+if numero > 0:
+    print("O número é positivo.")
+elif numero < 0:
+    print("O número é negativo.")
+else: numero == 0
+print("O número é zero.")
+
+        
+print ('............................................................................................................')
+
+
+...
+
+
+print ('2- Peça para o usuário digitar a idade, verifique se uma pessoa pode votar com  base na idade.')
+
+print(input('DIGITE SUA IDADE')),
+
+
+
+idade = 18  
+if idade <= 18 and idade >= 60:
+    print("Você é maior de idade. Vote concientemente.")
+
+
+else: idade and 17
+print("Você não é maior de idade.")
+
+
+print ('............................................................................................................')
+
+...
+
+print ('3 - Declara uma variável com um número qualquer, determine se um número é par ou ímpar.')
+
+
+
+
+
+
+
+
+print ('4 - Usuário vai digitar 3  números, para criar um triângulo, verifique se um triângulo é equilátero, isósceles ou escaleno.')
+
+...
+print ('Um triângulo é chamado de equilátero se todos os lados possuem a mesma medida.'),
+print('triângulo é chamado de isósceles se dois lados possuem a mesma medida.'),
+print('Um triângulo é chamado de escaleno se todos os lados possuem medidas diferentes.'),
+
+
+
+
+print ('............................................................................................................')
+
+
+
+
+print('5 - Determine se um número é múltiplo de 5 e 7.')
+
+	
+
+print ('............................................................................................................')
+
+
+
+print('6 - Verifique se um número é positivo e maior que 10')
+
+
+print(input('DIGITE UM NUMERO')),
+
+
+numero = 10
+
+
+if numero > 1:
+    print("O número é positivo.")
+elif numero < 0:
+    print("O número é negativo.")
+
+
+
+print ('............................................................................................................')
+
+
+print('7 - Verifique se um número é divisível por 3 ou 5.')
+
+
+numero = 13
+
+if numero / 5 or 7:
+
+elif numero < 13:
+
+
+
+
+
+
+
+print ('............................................................................................................')
+
+
+print('Fim AULA 8, Vamos para proxima fase!?')
