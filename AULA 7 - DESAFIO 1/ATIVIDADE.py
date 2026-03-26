10c02c2ff5c74a1a8a71110028914b41a34dbf6e
REPESKA <repeskaa@gmail.com>
2026-03-26 16:19:19 -0300

    Add files via upload

---
+++ AULA 7 - DESAFIO 1/ATIVIDADE.py
@@ -0,0 +1,34 @@
+
+ecommerce = {
+
+'prod1 ':'Carros',
+'prod2':'Motos',
+'prod3': 'Drones'
+
+}
+
+
+
+
+ecommerce = {
+
+
+'carros':{
+    'Mercedes': ['gla45', 'cla', 'c63'],
+    'BMW': ['x1', 'x3', 'x5'],
+    'Audi': ['a3', 'a4', 'a6']
+    
+    },
+
+
+'Motos':['Kawasaki', 'Ducati','Harley daivson'],
+
+
+'Drones': ['DJI 4F', 'Matrice 30T', 'Avata2']
+
+}
+
+print(ecommerce['carros']['mercedes']input[2]) 
+print(ecommerce['Motos']['bugati']input[1])
+print(ecommerce['Drones']['DJI 4F']input[1])
+lucid = float(input('Digite a segunda : ')) 
