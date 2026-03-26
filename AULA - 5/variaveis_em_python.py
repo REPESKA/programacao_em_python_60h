337389de7616e24dd759e2194fb191d3333b5f28
REPESKA <repeskaa@gmail.com>
2026-03-26 16:18:28 -0300

    Add files via upload

---
+++ AULA - 5/variaveis_em_python.py
@@ -0,0 +1,103 @@
+
+#variaveis
+
+# 4 tipos de dados primitivos
+
+# string - dados do tipo texto -" Texto"
+# float - dados do tipo decimal - 5.0
+# bool - dados do tipo booleano - True ou False
+# int - dados do tipo inteiro - 10 1 -2 0
+
+# variaveis
+
+# nome semântico - comecar por letra, snake_case (palavra composta)
+# Não começar por número ou caractere especial
+# estrutura de dados - lista, tupla, dicionário, conjunto
+
+
+nome = "Eloisa"
+sobrenome = "Silva"
+idade = 30
+endereco = "Rua das Flores, 123"
+peso = 65.5
+solteira = False
+
+
+
+print(nome)
+print(sobrenome)
+print(idade)
+print(endereco)
+print(peso)
+print(solteira)
+
+print('Idade:', idade)
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Endereço:', endereco)
+print('Peso:', peso)
+print('Solteira:', solteira)
+
+
+
+
+n1 = 10
+n2 = 5
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+print('Soma:', soma)
+print('Subtração:', subtracao)
+print('Multiplicação:', multiplicacao)
+print('Divisão:', divisao)
+
+
+
+
+nome = input("Nome: ")
+sobrenome = input("Sobrenome: ")
+idade = int(input("Idade: "))
+endereco = input("Endereço: ")
+peso = float(input("Peso: "))
+solteira = input("Solteira (True/False): ") == "True"
+
+
+# pendas
+
+#
+
+
+
+
+print (10 + 5)
+print (10 - 5)
+print (10 * 5)
+print (10 / 5)
+print (10 // 5) #int = int | dec // in = dec
+print (10 % 5)
+print (10 ** 5) #potência
+
+
+
+# input
+# pendências
+# sinais aritméticos
+# sinais logicos
+
+print (10 < 1) # menor que - false
+print (10 > 1) # maior que - true
+print (10 == 1) # igual - false
+print (10 != 1) # diferente - true
+print (10 <= 1) # menor ou igual - false
+print (10 >= 1) # maior ou igual - true
+
+
+
+# print (pulAR LINHA)
+print()
+\n
+print(f"{nome}  Mundo!")
+
+
