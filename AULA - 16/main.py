80dd11f7ed8dbbe2b11da93399c3f2d1e4b6f95b
REPESKA <repeskaa@gmail.com>
2026-03-27 17:56:01 -0300

    Add files via upload

---
+++ AULA - 16/main.py
@@ -0,0 +1,49 @@
+import pygame    
+
+
+
+pygame.init()
+tela  =  pygame.display.set_mode((500,500))
+run = True
+
+
+while run:
+
+
+    for event in pygame.event.get():
+        if event.type  == pygame.QUIT:
+            run = False
+            
+    tela.fill((83,20,205)) 
+    pygame.rect(tela,('affff'),(235,235,30,20))
+    pygame.display.update()    
+pygame.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
