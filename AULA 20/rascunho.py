44763a9a2f7337d9f334f54dbf8d058f772941f1
REPESKA <repeskaa@gmail.com>
2026-04-08 17:59:11 -0300

    Add files via upload

---
+++ AULA 20/rascunho.py
@@ -0,0 +1,29 @@
+1. SITUAÇÃO PROBLEMA: CADASTRO DE CLIENTES EM UM COMÉRCIO DE
+
+VAREJO
+
+A EMPRESA "XYZ COMÉRCIO" TEM DIFICULDADES EM CONTROLAR O
+
+CADASTRO DE SEUS CLIENTES. ATUALMENTE, O ARQUIVO COM OS DADOS
+
+DOS CLIENTES ESTÁ DESORGANIZADO, E A EQUIPE DE VENDAS TEM
+
+ENCONTRADO DIFICULDADES EM ENCONTRAR INFORMAÇÕES RÁPIDO. A
+
+EMPRESA PRECISA DE UM SISTEMA QUE PERMITA O CADASTRO DE NOVOS
+
+CLIENTES, A CONSULTA DE CLIENTES JÁ CADASTRADOS E A EDIÇÃO OU
+
+EXCLUSÃO DE DADOS.
+
+Solução proposta: Criar um sistema que permita o cadastro de novos clientes
+
+com informações como nome, e-mail, telefone e endereço. Além disso, o
+
+sistema permitirá a consulta, edição e exclusão dos dados dos clientes
+
+através de uma interface gráfica simples.
+
+
+
+
